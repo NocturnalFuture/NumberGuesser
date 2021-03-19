f07b646818b341fc9521f37cae1734f8be8d1525
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2021-03-19 13:39:08 +0100

    reDid entire Project and also added some SetTimeOut & counterUp package

--- app.js
+++ app.js
@@ -1,81 +1,137 @@
-let targetNumber = document.querySelector('.targetNumber');
-let targetScoreComputer = document.querySelector('.targetScoreComputer');
-let targetScoreUser = document.querySelector('.targetScoreUser');
-let userNumberInput = document.querySelector('.userNumberInput');
-let incrementButton = document.querySelector('.incrementButton');
-let decrementButton = document.querySelector('.decrementButton');
-let placeBet = document.querySelector('.placeBet');
-let computerNumberInput = document.querySelector('.computerNumberInput');
-let newRound = document.querySelector('.newRound');
+import counterUp from './node_modules/counterup2/index.js';
+
+// Variables
+let targetNumber = document.getElementById('targetNumber');
+let userInputDisplay = document.getElementById('userInputDisplay');
+const incrementButton = document.querySelector('.incrementButton');
+const decrementButton = document.querySelector('.decrementButton');
+const guessButton = document.querySelector('.guessButton');
+let computerPick = document.querySelector('.computerPick');
+let computerScoreCount =  document.querySelector('.computerScoreCount');
+let computerEmoji = document.querySelector('.computerEmoji');
+const nextRoundButton = document.querySelector('.nextRoundButton');
 let roundNumber = document.querySelector('.roundNumber');
+const userScoreCount = document.querySelector('.userScoreCount');
+// Generate Target Number
+const randomTargetNumber = () => {
+    return Math.floor(Math.random()*10)
+}
+
+// Generate Random Number
+const randomNumberGenerator = () => {
+    return Math.floor(Math.random()*10);
+}
 
 
-// Button Handlers
+// Button Handle Logic
 incrementButton.addEventListener('click', () => {
-    userNumberInput.innerHTML++;
+    userInputDisplay.textContent++;
+    if(userInputDisplay.textContent >= 10 ){
+        userInputDisplay.textContent = 10;
+    }
+
 })
 
 decrementButton.addEventListener('click', () => {
-    userNumberInput.innerHTML--;
+    userInputDisplay.textContent--;
+    if(userInputDisplay.textContent  <= 0 ){
+        userInputDisplay.textContent = 0;
+    }
 })
 
-// Target Number Generator
-const targetNumberGenerator = () => {
-    return Math.floor(Math.random()*10);
-}
+// Main Event Handler
 
-// AI Genertor
-const aiGuess = () => {
-    return Math.floor(Math.random()*10);
-}
+guessButton.addEventListener('click', () => {
+// Players Choices
 
-// Human Guess
-placeBet.addEventListener('click', () => {
+let aiGuess =  randomNumberGenerator();
+let numberTarget = randomTargetNumber();
+let userGuess = userInputDisplay.textContent;
 
-    // Players Choice
-        let humanBet = userNumberInput.innerHTML;
-        let aiBet = aiGuess();
-        let targetNum = targetNumberGenerator();
+ // Displaying Numbers
+ targetNumber.textContent = numberTarget;
+ computerPick.textContent = aiGuess;
 
+ counterUp( computerPick, {
+    duration: 1000,
+    delay: 16,
+} )
 
-    console.log(`Computer picks: ${aiBet}`)
+//Result Logic
+const userTargetGuess = Math.abs(numberTarget - userGuess)
+const computerTargetGuess = Math.abs(numberTarget - aiGuess)
 
-/*      
-console.log('Humans bet is:', humanBet);
-console.log('Ais bet is:', aiBet);
-console.log('Target number is:', targetNum);
-console.log('PlaceHolder Currently Holds', computerTargetNumber)
-*/
-roundNumber.innerHTML++;
-
-computerNumberInput.innerHTML = aiBet;
-
-const userTargetGuess = Math.abs(targetNum - humanBet)
-const computerTargetGuess = Math.abs(targetNum - aiBet)
-targetNumber.innerHTML = `<h1>${targetNum}</h1>`;
-console.log('Human distance is', userTargetGuess);
-console.log('Computer distance is', computerTargetGuess);
-
+setTimeout(function(){
 if(userTargetGuess < computerTargetGuess){
-    placeBet.innerHTML =  '<h1 style="color:#3dd790">User wins</h1>';
-    targetScoreUser.innerHTML ++;
+    guessButton.innerHTML =  "<h1 style=color:white>User wins</h1>";
+    userScoreCount.textContent++;
   } else if(computerTargetGuess < userTargetGuess) {
-    placeBet.innerHTML =  '<h1 style="color:#fe3b77">AI wins</h1>';
-    targetScoreComputer.innerHTML ++;
+    guessButton.innerHTML =  '<h1 style=color:white>AI wins</h1>';
+    computerScoreCount.innerHTML ++;
+    computerEmoji.textContent = "🤖"
   } else if(userTargetGuess === computerTargetGuess) {
-    placeBet.innerHTML =  '<h1>Draw</h1>'
+    guessButton.innerHTML  = "<h1 style=color:white>Draw</h1>";
   }
+},1800);
+
 })
 
 
-
-newRound.addEventListener('click', () => {
-    computerNumberInput.innerHTML = 0;
-    userNumberInput.innerHTML = 0;
-    targetNumber.innerHTML = '<h1>?</h1>';
-    placeBet.innerHTML = '<h1>Fire Off Guess</h1>';
+// New Round Logic
+nextRoundButton.addEventListener('click', ()=> {
+    computerPick.textContent = 0;
+    userInputDisplay.textContent = 0;
+    computerEmoji.textContent = ""
+    guessButton.innerHTML="<h1>Guess</h1>"
+    roundNumber.textContent++;
+    targetNumber.textContent = "?"
 })
 
 
+// Cursos Modification
+var cursor = $(".cursor"),
+                         follower = $(".cursor-follower");
 
+                     var posX = 0,
+                         posY = 0,
+                         mouseX = 0,
+                         mouseY = 0;
 
+                     TweenMax.to({}, 0.016, {
+                         repeat: -1,
+                         onRepeat: function() {
+                             posX += (mouseX - posX) / 9;
+                             posY += (mouseY - posY) / 9;
+
+                             TweenMax.set(follower, {
+                                 css: {
+                                     left: posX - 20,
+                                     top: posY - 20
+                                 }
+                             });
+
+                             TweenMax.set(cursor, {
+                                 css: {
+                                     left: mouseX,
+                                     top: mouseY
+                                 }
+                             });
+                         }
+                     });
+
+                     $(document).on("mousemove", function(e) {
+                         mouseX = e.pageX;
+                         mouseY = e.pageY;
+                     });
+
+                     $(".portfolio-item img").on("mouseenter", function() {
+                         cursor.addClass("active");
+                         follower.addClass("active");
+                     });
+
+                     $(".portfolio-item img").on("mouseleave", function() {
+                         cursor.removeClass("active");
+                         follower.removeClass("active");
+                     });
+
+                 
